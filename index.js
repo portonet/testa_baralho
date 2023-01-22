14fb209d399b14f1bcf58136549b04c9ca2214b3
Aline <43306767+portonet@users.noreply.github.com>
2023-01-21 22:23:04 -0300

    commit 1

---
+++ index.js
@@ -0,0 +1,17 @@
+import random *
+
+# naipes = ["paus", "ouros", "espadas", "copas"];
+
+# This program asks for your nome and uses
+
+# it in an output message.
+
+# Run the program here by typing "Ctrl R"
+
+ 
+
+nome=input("What's your name? ")
+
+print("Hello, ", nome)
+
+print("\n Press ctrl+R to run again")
